acabd6b0cd91086c47ab2126b3d08ae3861e4a65
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-11 06:52:11 +0500

    added local time

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const monthName = document.querySelector("#month-name"),
+const monthNameEl = document.querySelector("#month-name"),
   dayNameEl = document.querySelector("#day-name"),
   dayNumEl = document.querySelector("#day-number"),
   yearEl = document.querySelector("#year");
+
+const date = new Date();
+const month = date.getMonth();
+
+monthNameEl.innerText = date.toLocaleString("en", {
+  month: "long",
+});
+
+dayNameEl.innerText = date.toLocaleString("en", {
+  weekday: "long",
+});
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();

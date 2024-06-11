fb785fd4cc1fb790f12108a1d7e498254e176467
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-11 06:35:37 +0500

    added calendar style

---
+++ script.js
@@ -0,0 +1,4 @@
+const monthName = document.querySelector("#month-name"),
+  dayNameEl = document.querySelector("#day-name"),
+  dayNumEl = document.querySelector("#day-number"),
+  yearEl = document.querySelector("#year");
